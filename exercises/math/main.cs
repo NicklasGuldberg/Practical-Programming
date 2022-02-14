bd14c532720eefa74afd5f451cd045c9c2998a92
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-02-14 01:03:14 +0100

    Finished the math-exercise

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -1,6 +1,17 @@
 using static System.Console;
 using static System.Math;
-static class Main(){
-Write("test");
-return 0;
+
+class math{
+	static void Main(){
+		double sqrt2 = Sqrt(2.0);
+		double epi = Pow(E, PI);
+		double pie = Pow(PI, E);
+		Write($"sqrt(2) = {sqrt2}\n");
+		Write($"Test of sqrt(2): 2 = {sqrt2*sqrt2}\n");
+		Write($"e^pi = {epi}\n");
+		Write($"test for e^pi: 1 = {Pow(epi,1/PI)/E}\n");
+		Write($"pi^e = {pie}\n");
+		Write($"test for pi^e: 1 = {Pow(pie,1/E)/PI}\n");
+	}
+
 }
