4ca4b07822cae464c8760dd44be1eca650405b42
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-02-14 03:53:58 +0100

    Did the while loop for both min and max rep int.

--- exercises/epsilon/epsilon.cs
+++ exercises/epsilon/epsilon.cs
@@ -6,5 +6,9 @@ class epsilon{
 		int i=1; while(i+1>i) {i++;}
 		Write($"Maximum representable integer is {i}\n");
 		Write($"int.MaxValue is {int.MaxValue}\n");
+		
+		int j=1; while(j-1<j) {j--;}
+		Write($"Minimum representable integer is {j}\n");
+		Write($"int.MinValue is {int.MinValue}\n");
 	}
 }
