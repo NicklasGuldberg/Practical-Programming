d0541a06bb6942c6abf7aa2d624cbad5b5460c08
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-02-17 19:11:55 +0100

    Calculated all the complex numbers.

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -1,8 +1,29 @@
 using System;
 using static System.Console;
 using static System.Math;
+using static cmath;
+
 class main{
     static void Main(){
-        WriteLine("Test 3");
+        //double a = - 1;
+        //complex i = new complex(1,2);
+        complex a = sqrt(new complex(-1,0));
+        complex b = sqrt(I);
+        complex c = exp(I);
+        complex d = exp(I*PI);
+        complex e = I.pow(I);
+        complex f = log(I);
+        complex g = sin(I*PI);
+        WriteLine($"sqrt(-1) = {a}");
+        WriteLine($"i = {a} is {a.approx(I)}");
+        WriteLine($"sqrt(i) = {b}");
+        WriteLine($"exp(i) = {c}");
+        WriteLine($"exp(i*pi) = {d}");
+        WriteLine($"-1 = {d} is {d.approx(-1)}");
+        WriteLine($"i^i = {e}");
+        WriteLine($"log(i) = {f}");
+        WriteLine($"sin(i*pi) = {g}");
+
+
     }
 }
