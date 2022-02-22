c38a6a1906984742c6717dc2453384cece3d8760
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-02-22 17:18:18 +0100

    Added comments

--- Homework/generic-list/genlist.cs
+++ Homework/generic-list/genlist.cs
@@ -2,11 +2,15 @@ using System;
 using static System.Math;
 using static System.Console;
 
+/*list.data is an array of 'T', where T is the type. 
+Thus if the type is array, we would get an array of arrays 
+- a so-called 'jagged array'
+*/
 public class genlist<T>{
-    public T[] data;
-    public int size {get{return data.Length;}} 
-	public genlist(){ data = new T[0]; }
-	public void push(T item){ //push appends an entry to the list
+    public T[] data; 
+    public int size {get{return data.Length;}} //get makes the size property read-only. 
+	public genlist(){data = new T[0]; } //constructor
+	public void push(T item){ //push method appends an entry to the list. Entries can be arrays. 
 		T[] newdata = new T[size+1];
 		for(int i=0;i<size;i++)newdata[i]=data[i];
 		newdata[size]=item;
