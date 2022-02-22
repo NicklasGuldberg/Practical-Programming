74ae40c2bb614c5a02ffcc9cd9cc90ef7d08e99c
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-02-22 14:49:33 +0100

    Completed the exercise

---
+++ exercises/passing-functions/main.cs
@@ -0,0 +1,17 @@
+using System;
+using static System.Console;
+using static System.Math;
+using static table;
+class main{
+    static void Main(){
+        double a = 0;
+        double b = 2*PI;
+        double dx = 0.5;
+        double k = 0;
+        System.Func<double,double> f = delegate(double x){return Sin(k*x);};
+        for (int i=1; i<=3; i++){
+            k = i;
+            make_table(f,a,b,dx);
+        } 
+    }
+}
