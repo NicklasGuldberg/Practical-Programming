abf139feb4db36c2c4603f3cb18de594549ac180
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-03-31 16:41:04 +0200

    Plot of random points with qspline works

---
+++ Homework/splines/B/main.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using static System.Console;
+using static System.Math;
+using static qspline;
+class main{
+    static void Main(){
+        Random rnd = new Random();
+        int n = 10;
+        double xmin = 0;
+        double xmax = 5;
+        var x = new double[n];
+        var y = new double[n];
+        for(int i=0; i<n; i++){
+            x[i] = xmin + i/Convert.ToDouble(n) * (xmax - xmin);
+            y[i] = rnd.Next(0,5);
+            WriteLine($"{x[i]} {y[i]}");
+        }
+        WriteLine();
+        WriteLine();
+        // Two empty lines for another plot.
+        qspline qs = new qspline(x,y);
+        for(double i = qs.x[0]; i<qs.x[qs.n-1]; i += 0.01){
+            WriteLine($"{i} {qs.spline(i)}");
+        }
+    }
+}
