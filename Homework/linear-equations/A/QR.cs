53b90a63641de6522e3910ed643221218bdb424a
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-04-01 16:42:16 +0200

    Now solves the system of linear equations

--- Homework/linear-equations/A/QR.cs
+++ Homework/linear-equations/A/QR.cs
@@ -6,22 +6,37 @@ using static matrix;
 
 public class QRGS{
     public matrix Q,R;
+    public int n,m;
 	public QRGS(matrix A){
-        int n = A.size1;
-        int m = A.size2;
+        n = A.size1;
+        m = A.size2;
         vector[] a = new vector[m]; // m is the number of vectors - not the size of each vector!
         vector[] q = new vector[m];     
         Q = new matrix(n,m);
+        R = new matrix(n,m);
         for(int i = 0; i<m; ++i) a[i] = A[i];
         for(int i = 0; i<m; ++i){
             q[i] = a[i]/a[i].norm();
-            // for(int j=i+1; j<m; ++j){
-            //     a[j] = a[j] - q[i]%a[j] * q[i];
-            // }
+            for(int j=i+1; j<m; ++j){
+                a[j] = a[j] - q[i]%a[j] * q[i];
+            }
             Q[i] = q[i];
+        R = Q.transpose()*A;
         }
     }
-	// public vector solve(vector b){/* back-substitute Q^T*b */}
+	public vector solve(vector b){
+        if(!(n==m)) throw new Exception("Not a square matrix");
+        vector c = Q.transpose()*b;
+        matrix U = R; 
+        vector y = new vector(n);
+        double sum = 0;
+        for(int i=n-1; i>=0; --i){
+            sum = 0;
+            for(int k = i + 1; k < n; ++k) sum += U[i,k] * y[k];
+            y[i] = 1/U[i,i] * (c[i] - sum);
+        }
+        return y;
+    }
 	// public matrix inverse(){/* return the inverse matrix (part B) */}
 	// public double determinant(){/* return ΠiRii */}
 }
