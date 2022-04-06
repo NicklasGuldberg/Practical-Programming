ef16eb3b58ec9fd61e913dc8964a05b1bfff9b4a
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-04-06 02:37:53 +0200

    Added template

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using static System.Console;
+using static System.Math;
+using static matrix;
+class main{
+    static void Main(){
+        
+   }    
+}
