2b7868f08b2427cdb075926adeb685386ccce0a5
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-04-07 16:29:10 +0200

    Implemented cyclic method + overload to store V

--- Homework/eigenvalues/A/eigenvalues.cs
+++ Homework/eigenvalues/A/eigenvalues.cs
@@ -36,19 +36,33 @@ public static class eigenvalues{
                 double Aqq = A[q,q];
                 double Apq = A[p,q];
                 double theta = 0.5*Atan2(2*Apq,Aqq-App);
-                WriteLine($"{p}, {q}");
                 Jtimes(A,p,q,-theta);
                 timesJ(A,p,q,theta);
             }
         }
     }
-    public static void jcyclic(matrix A){
+    public static void sweep(matrix A, matrix V){
+        for(int p = 0; p < A.size1; ++p){ 
+            for(int q = p + 1; q < A.size1; ++q){
+                double App = A[p,p];
+                double Aqq = A[q,q];
+                double Apq = A[p,q];
+                double theta = 0.5*Atan2(2*Apq,Aqq-App);
+                Jtimes(A,p,q,-theta);
+                timesJ(A,p,q,theta);
+                timesJ(V,p,q,theta);
+            }
+        }
+    }
+
+    public static void jcyclic(matrix A, matrix V){
         vector Diag1 = new vector(A.size1);
         vector Diag2 = new vector(A.size1);
+        V.set_identity();
         do
         {
         for(int i = 0; i< A.size1; ++i) Diag1[i] = A[i,i];
-        sweep(A);
+        sweep(A,V);
         for(int i = 0; i< A.size1; ++i) Diag2[i] = A[i,i];
         }
         while(!Diag1.approx(Diag2));
