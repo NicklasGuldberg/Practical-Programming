746009bb6cc775a750527134860c501754eb4267
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-04-19 16:48:13 +0200

    ODE works now

--- Homework/ODE/A/ODE.cs
+++ Homework/ODE/A/ODE.cs
@@ -3,7 +3,45 @@ using System.Collections.Generic;
 using static System.Console;
 using static System.Math;
 using static matrix; 
-
+using static vector;
 public static class ODE{
-    
+    public static (vector,vector) rkstep23(Func<double,vector,vector> f, double x, vector y, double h){
+        //Bogacki-Shampine method   (does not utilize FSAL (First Same As Last))
+        vector k0 = f(x,y);
+        vector k1 = f(x + 0.5 * h, y + 0.5*h*k0);
+        vector k2 = f(x + 0.75 * h, y + + 0.75*h*k1);
+        vector k3 = f(x + h, y + 2.0/9*h*k0 + 1.0/3*h*k1 + 4.0/9*h*k2); 
+        vector k = 2.0/9 * k0 + 1.0/3 * k1 + 4.0/9 * k2;
+        vector kstar = 7.0/24 * k0 + 0.25 * k1 + 1.0/3 * k2 + 1.0/8 * k3;
+        vector yh = y + h * k;
+        vector err = h*(k - kstar);
+        return (yh,err);
+    }
+    public static vector driver(
+        Func<double,vector,vector> f, 
+        double a, 
+        vector ya, 
+        double b, 
+        double h=0.01, 
+        double acc=0.01, 
+        double eps = 0.01){
+        //this driver compares all vectors elements and not just the norm.
+        if(a>b) throw new Exception("Driver: a>b");
+        double x = a;
+        vector y = ya;
+        double frac;
+        do {
+            if(x>=b) return y;
+            if(x + h > b) h = b-x; 
+            var (yh, err) = rkstep23(f, x, y, h);
+            double Tol = Min(acc, yh.norm()*eps) * Sqrt(h/(b-a)) ;
+            double Err = err.norm();
+            if(Err <= Tol){
+                x += h;
+                y = yh;
+                // WriteLine($"{x}, {y[0]}");
+            }
+            if(Err > 0) h *= Pow(Tol/Err, 0.25) * 0.95; 
+        }while(true);
+    }
 }
--- Homework/ODE/A/main.cs
+++ Homework/ODE/A/main.cs
@@ -4,7 +4,32 @@ using static System.Console;
 using static System.Math;
 using static matrix;
 class main{
+    static Func<double,vector,vector> f = delegate(double x, vector y){
+            return new vector(y[1], -y[0]);
+        };
+    
+    static Func<double,vector,vector> pend = delegate(double x, vector y){
+        //We're looking at the second order DE, theta''(t) + b*theta'(t) + c*sin(theta(t)) = 0 
+        double theta = y[0];
+        double omega = y[1];
+        double b = 0.25;
+        double c = 5.0; 
+        return new vector(omega, -b*omega - c * Sin(theta));
+    };
     static void Main(){
-        WriteLine("Hello World!");        
+
+        //Example with u'' = -u
+        vector ya = new vector(0.0, 1.0);
+        double N = 50;
+        for(int i = 0; i<N; ++i){
+            double x = 2*PI * i/N;
+            vector y = ODE.driver(f,0,ya,x);
+            WriteLine($"{x}, {y[0]}");
+        }
+        WriteLine();
+        WriteLine();
+
+        //Example from 
+        
    }    
 }
