5a6d31eac64f93b0a821b4b546472e9bc1015741
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-04-26 17:22:07 +0200

    Added rosenbock function

--- Homework/root-finding/A/main.cs
+++ Homework/root-finding/A/main.cs
@@ -15,12 +15,33 @@ class main{
         };
         vector x0 = new vector(2,3);
         var x = newton(f,x0);
+
+        Func<vector,vector> F = delegate(vector v){
+            double F1 = 2*v[0] + 400*v[0]*v[0]*v[0] - 400*v[0]*v[1] - 2;
+            double F2 = 200 * (v[1] - v[0]*v[0]);
+            vector result = new vector(F1,F2);
+            return result;
+        };
+        vector Fx0 = new vector(5,5);
+        vector Fx = newton(F,Fx0);
+
         WriteLine("\n");
         WriteLine("As a test we solve the simple system f(x,y) = [x^2 - 2y, y^2 - 3x] ");
-        Write("The solution is  ");
+        Write("The guess is ");
+        x0.print("     x0  = ");
+        Write("The solution is ");
         x.print("[x,y] = ");
         // WriteLine();
-        WriteLine($"inserting it back into the function gives us f([x,y]) = [{f(x)[0]}, {f(x)[1]}] ");
+        WriteLine($"Inserting it back into the function gives us f(x,y) = [{f(x)[0]}, {f(x)[1]}] ");
+        WriteLine();
+
+        WriteLine("Now we find the extremum of f(x,y) = (1-x)^2 + 100(y - x^2)^2");
+        WriteLine("The gradient is gradf(x,y) = (2x + 400x^3 - 400xy -2,  200(y-x^2)) ");
+        Write("The guess is ");
+        Fx0.print("     x0  =");
+        Write("The solution is ");
+        Fx.print("[x,y] =");
+        WriteLine("According to wikipedia the correct minimum is (1,1)");
         WriteLine();
    }    
 }
