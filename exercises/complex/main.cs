aeaa36b033729d3b4f5cf4785616352c192af1d4
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-05-02 12:15:49 +0200

    used 'make'

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -23,10 +23,14 @@ class main{
         WriteLine($"exp(i*pi) = {d}");
         WriteLine($"-1 = {d} is {d.approx(-1)}");
         WriteLine($"i^i = {e}");
+        WriteLine($"i^i = exp(- pi/2) is {e.approx(Exp(-PI/2.0))}");
         WriteLine($"log(i) = {f}");
+        WriteLine($"log(i) = pi/2*i is {f.approx(PI/2.0 * I)}");
         WriteLine($"sin(i*pi) = {g}");
-        WriteLine($"sinh(i) = {h}");
-        WriteLine($"cosh(i) = {i}");
+        WriteLine($"sin(i*pi) = i*sinh(pi) is {g.approx(I*Sinh(PI))}");
+        // WriteLine($"sinh(i) = {h}");
+        // WriteLine($"sinh(i) = i*sin(1) is {h.approx(I*sin(1))}");
+        // WriteLine($"cosh(i) = {i}");
 
     }
 }
