5c4ace9d305a4181f115640f9f895026bdb36ce7
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-06-24 04:42:48 +0200

    Works now, but takes a lot of steps

--- Homework/minimization/A/main.cs
+++ Homework/minimization/A/main.cs
@@ -6,28 +6,47 @@ using static matrix;
 using static minimizer;
 class main{
     static void Main(){
-        WriteLine("Hello World");        
 	Func<vector, double> f = delegate(vector v){
 		double x = v[0];
 		double y = v[1];
 		return Pow((1-x),2) + 100*Pow(y-x*x,2);
 	};
-	Func<vector,double> h = delegate(vector v){
-		return (v[0]-2)*(v[0]-2) + (v[1] - 3)*(v[1] - 3);
-	};
-
-	vector v0 = new vector(0.1, 0.1); //Everything breaks when the start guess is 0. Not sure why??
-	vector vopt = qnewton(f, v0, 0.00001);
-	vopt.print("vopt =");
-	var test = gradient(f,vopt);
-	test.print("grad(vopt) =");
-
-
 	Func<vector,double> g = delegate(vector v){
 		double x = v[0];
 		double y = v[1];
 		return Pow(x*x + y -11, 2) + Pow(x+y*y -7,2);
 	};
+	Func<vector,double> h = delegate(vector v){
+		return (v[0]-2)*(v[0]-2) + (v[1] - 3)*(v[1] - 3);
+	};
+
+	WriteLine();
+	WriteLine("Rosenbrock's valley function");
+	WriteLine("-------------------------");
+	vector v0 = new vector(0.0, 0.0); //Now works for 0,0 guess too !! !
+	var (vopt,steps) = qnewton(f, v0, 0.0001);
+	WriteLine("f(x,y) = (1-x)^2 + 100(y-x^2)^2");
+	v0.print("Start guess: (x0,y0)	= ");
+	vopt.print("Minimum found at (x,y)	= ");
+	WriteLine($"Steps taken: {steps}");
+	//var test = gradient(f,vopt);
+	//test.print("grad(vopt) =");
+	WriteLine("According to wikipedia minimum should be at (x,y) = (1,1)");
+	WriteLine("------------------------");
+
+
+	WriteLine();
+	WriteLine("Himmelblau's function");
+	WriteLine("-------------------------");
+	var (vopt2,steps2) = qnewton(g, v0, 0.0001);
+	WriteLine("g(x,y) = (x^2 + y - 11)^2 + (x + y^2 - 7)^2");
+	v0.print("Start guess: (x0,y0)	= ");
+	vopt2.print("Minimum found at (x,y)	= ");
+	WriteLine($"Steps taken: {steps2}");
+	//var test = gradient(f,vopt);
+	//test.print("grad(vopt) =");
+	WriteLine("According to wikipedia minimum exist at (x,y) = (3,2), (-2.805, 3.131), (-3.779,-3.283) and (3.584,-1.848)");
+	WriteLine("------------------------");
 
 
 
--- Homework/minimization/A/minimize.cs
+++ Homework/minimization/A/minimize.cs
@@ -24,29 +24,31 @@ public static class minimizer{
 		vector dx = new vector(n);
 		dx.set_zero();
 		for(int i = 0; i<n; ++i){
-			dx[i] = Min(Abs(x[i])*Pow(2,-26), Pow(2,-27)); 
+			dx[i] = Max(Abs(x[i])*Pow(2,-26), Pow(2,-26)); 
 			df[i] = (f(x + dx) - f(x))/ dx[i];
 			dx[i] = 0;
 		}
 		return df;
 	}
 
-	public static vector qnewton(Func<vector,double> f, vector start, double acc=0.0001){
+	public static (vector, int) qnewton(Func<vector,double> f, vector start, double acc=0.0001){
 		int n = start.size;
+		int steps = 0;
 		matrix B = new matrix(n,n);
 		B.set_unity();
 		vector x = start;
 		vector df = gradient(f,x); 
 		vector deltax = new vector(n);
 
-		do{
+		while(df.norm()>acc){
+			++steps;
 			deltax = -B * df;
 			double lambda = 1;
-			while( !(f(x + lambda* deltax) < f(x)) & lambda > 0.001){
+			while( lambda > Pow(2,-10) & !(f(x + lambda* deltax) < f(x)) ){
 				lambda *= 0.5;
 			};
 			vector s = lambda*deltax; //This is the step
-			if (lambda > 1.0/32) {
+			if (lambda > Pow(2,-10)) {
 				//if lambda is not too small and f(x + lambda*deltax) < f(x) we get to here
 				//now for the update
 				B += SR1(f,x,s,B,df); 
@@ -57,8 +59,8 @@ public static class minimizer{
 				B.set_unity();
 				df = gradient(f,x); //This is done last df is not updated at the start of the while loop.
 			}
-		} while(df.norm()>acc);
-		return x;
+		} 
+		return (x,steps);
 	}
 
 
