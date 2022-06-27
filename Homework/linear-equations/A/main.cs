3a94c9dbbf741c3483d232b053fc1457f828b6d3
Nicklas Guldberg <nicklas.gberg@gmail.com>
2022-06-27 19:15:18 +0200

    Added tall matrix and some tests

--- Homework/linear-equations/A/main.cs
+++ Homework/linear-equations/A/main.cs
@@ -6,35 +6,58 @@ using static matrix;
 class main{
     static void Main(){
         int n = 5;
-        int m = 5;
+        int m = 8;
         int min = 0;
         int max = 10;
         Random rnd = new Random();
-        matrix A = new matrix(n,m);
+        var I = new matrix(n,n);
+        I.set_unity();
+
+
+        matrix A1 = new matrix(m,n);
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                A1[i,j] = rnd.Next(min,max);
+            }
+        }
+        var QR1 = new QRGS(A1);
+        WriteLine();
+        WriteLine("First we test the tall matrix");
+        WriteLine("In the interest of not printing the same matrix multiple times the approx() function is used for testing");
+        WriteLine("A =");
+        A1.print();
+        Write("Q =");
+        QR1.Q.print();
+        Write("R =");
+        QR1.R.print();
+        WriteLine($"Q^T * Q = 1: {I.approx(QR1.Q.transpose()*QR1.Q)}");
+        WriteLine($"Q * R = A: {A1.approx(QR1.Q * QR1.R)}");
+
+        WriteLine();
+        WriteLine("Now for the square matrix");
+        matrix A = new matrix(n,n);
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
+            for(int j = 0; j<n; j++){
                 A[i,j] = rnd.Next(min,max);
             }
         }
-        var I = new matrix(n,n);
-        I.set_unity();
         var QR = new QRGS(A);
         vector b = new vector(n);
         for(int i = 0; i < n; ++i){
             b[i] = rnd.Next(min,max);
         }
         vector x = QR.solve(b);
-        matrix B = QR.inverse();
         Write("A =");
         A.print();
-        Write("R =");
-        QR.R.print();
         Write("Q =");
         QR.Q.print();
-        Write("B =");
-        B.print();
-
-        WriteLine($"Q^T * Q = 1: {I.approx(QR.Q.transpose()*QR.Q)}");
+        Write("R =");
+        QR.R.print();
+        Write("b =");
+        b.print();
+        WriteLine("Solving for Q*R x=b for x yields");
+        Write("x =");
+        x.print();
         WriteLine($"Q * R = A: {A.approx(QR.Q * QR.R)}");
         WriteLine($"A * x = b: {b.approx(A*x)}");
    }    
